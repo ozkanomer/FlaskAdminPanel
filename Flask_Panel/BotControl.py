8f40dfed3ed4166b24c862ab87d545e6c7f1660e
Omer Faruk OZKAN <65222729+ozkanomer@users.noreply.github.com>
2021-08-23 16:38:03 +0300

    Update BotControl.py

--- Flask_Panel/BotControl.py
+++ Flask_Panel/BotControl.py
@@ -3,7 +3,7 @@ import requests, json
 # Bot or Human
 def is_Human(captcha_response):
 
-    secret = "6LfN9E0bAAAAAK4KOr2AeffxyKhqWGYJbGLmuO__"
+    secret = "Google ReCAPTCHA Secret Key"
     payload = {'response':captcha_response, 'secret':secret}
     response = requests.post("https://www.google.com/recaptcha/api/siteverify", payload)
     response_text = json.loads(response.text)
